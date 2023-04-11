11458a9f2356e313d2b223e81154e46d5d78e6cb
arge <omerrsenem@gmail.com>
2023-04-11 11:51:41 +0300

    fix: Authenticated router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import {createRouter, createWebHashHistory} from "vue-router";
-
-
+import store from "@/store";
 const routes = [
     {
     name: "HomePage",
@@ -29,7 +28,22 @@ const routes = [
 
 ]
 
-export default createRouter({
+
+const router= createRouter({
     routes,
     history:createWebHashHistory()
 })
+
+router.beforeEach((to,from,next) =>{
+    const authRequiredRoutes=["HomePage"]
+    const _isAuthenticated=store.getters._isAuthenticated
+    if(authRequiredRoutes.indexOf(to.name) > -1){
+        if(_isAuthenticated) next();
+        else next(false)
+    }else{
+        next();
+    }
+
+})
+
+export default router
